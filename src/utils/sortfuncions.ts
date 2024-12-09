5688df16b269c57b0a9aea92303e966d2bbb4b99
Pedro Colle <pedro.h.b.colle@gmail.com>
2024-12-09 15:08:21 -0300

    correção de bugs na ordenação e estilização do site

--- src/utils/sortfuncions.ts
+++ src/utils/sortfuncions.ts
@@ -1,13 +1,19 @@
 import type { CollectionEntry } from 'astro:content';
 
+function parseDate(d: string): Date{
+	let [day, month, year] = d.split('/') as any
+	const dateObj = new Date(+year, +month - 1, +day)
+	return dateObj
+}
+
 /**
  * ordena os post de forma descendente baseado na data de criação
  * @param posts : lista de posts
  */
 export function sortPosts(posts: CollectionEntry<'posts'>[]): void{
 	posts.sort(({data : d1}, {data: d2}) => {
-		const date1 = new Date(d1.createdAt)
-		const date2 = new Date(d2.createdAt)
-		return date2.getTime() - date1.getTime() 
+		const date1 = parseDate(d1.createdAt).getTime()
+		const date2 = parseDate(d2.createdAt).getTime()
+		return date2 - date1
 	})
 }
